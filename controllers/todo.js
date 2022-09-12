4d3c8541586c797e3a6f61f86434a8327d877e5d
Natalya Kulikovskaya <n.kulikovskaya@dunice.net>
2022-09-12 23:35:22 +0300

    add test from editTodo, deleteTodo, deleteCompleteTodo and checkAllTodo

--- controllers/todo.js
+++ controllers/todo.js
@@ -4,10 +4,10 @@ let Todo = require('../db/models/todo');
 const getTodos = (req, res) => {
     Todo.find()
         .then(function (data) {
-            res.send(data)
+            res.status(200).send(data)
         })
-        .catch(function () {
-            return res.status(400).send('Array is empty')
+        .catch(function (e) {
+            res.status(e.status).send(e)
         })
 
 };
@@ -17,13 +17,13 @@ const addTodo = (req, res) => {
   if(name){
     Todo.create({name: name})
       .then(function (data) {
-        res.send(data);
+        res.status(200).send(data);
       })
-      .catch(function () {
-        return res.status(400).send('Unable to add todo in list');
+      .catch( (e) => {
+        return res.status(e.status).send(e);
       })
   } else {
-     return res.status(403).send('Unable to create todo - empty name');
+     return res.status(403).send({message: 'Unable to create todo - empty name'});
   }
 
 };
@@ -33,7 +33,7 @@ const deleteTodo = (req, res) => {
     const id = req.params.id;
     Todo.findByIdAndRemove(id)
         .then(function (data) {
-            res.send(data);
+            res.status(200).send(data);
         })
         .catch(function () {
             return res.status(400).send('Todo was not found');
@@ -44,7 +44,7 @@ const deleteTodo = (req, res) => {
 const deleteCompleteTodo = (req, res) => {
     Todo.deleteMany({status: true})
         .then(function (data) {
-            res.send(data);
+            res.status(200).send(data);
         })
         .catch(function () {
             return res.status(400).send('Error delete all complete todos');
@@ -61,7 +61,7 @@ const editTodo = (req, res) => {
         $set: updateTodo,
     })
         .then(function (data) {
-            res.send(data);
+            res.status(200).send(data);
         })
         .catch(function () {
             return res.status(400).send('Todo was not found ');
@@ -71,10 +71,9 @@ const editTodo = (req, res) => {
 
 const checkAllTodo = (req, res) => {
     let status = req.body.status;
-    console.log('req.body', req.body)
     Todo.updateMany({}, {$set: {status: status}})
         .then(function (data) {
-            res.send(data);
+            res.status(200).send(data);
         })
         .catch(function () {
             return res.status(400).send('Todo was not found');
--- controllers/todo.test.js
+++ controllers/todo.test.js
@@ -10,6 +10,18 @@ const CREATE_SUCCESS_RESULT = {name: 'Learn Spanish', status: false, _id: '12dd-
 
 const FIND_BY_ID_SUCCESS_RESULT = {name: 'Learn Italian', status: true, _id: 'ssdd-1212zxz-12lllfg'};
 
+const DELETE_COMPLETE_TODO_SUCCESS_RESULT = [
+    {name: 'Learn English', status: false, _id: '12as-1212zxz-12asfg'},
+    {name: 'Learn Spanish', status: false, _id: '12dd-1212zxz-12lllfg'}
+]
+
+const EDIT_SUCCESS_RESULT = {name: 'Study JS', status: true, _id: '12dd-1212zxz-12lllfg'};
+
+const CHECK_ALL_STATUS_COMPLETE_TODO_SUCCESS_RESULT = [
+    {name: 'Learn English', status: true, _id: '12as-1212zxz-12asfg'},
+    {name: 'Learn Spanish', status: true, _id: '12dd-1212zxz-12lllfg'}
+]
+
 jest.mock('../db/models/todo', () => {
     const originalModule = jest.requireActual('../db/models/todo');
     mockF = jest.fn();
@@ -33,9 +45,10 @@ const mockRequest = () => {
 
 const mockResponse = () => {
     const res = { send: null, status: null };
-    res.send = jest.fn().mockReturnValue(res)
-    res.status = jest.fn().mockReturnValue(res)
-    return res
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
 };
 
 describe('controllers', () => {
@@ -51,37 +64,114 @@ describe('controllers', () => {
             const res = mockResponse();
             await todoControllers.getTodos(req, res);
             expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(200);
         })
-        it('controller return error if 200', async () => {
-            mockF.mockImplementation(() => Promise.reject('Array is empty'))
+        it('controller return error if 400', async () => {
+            mockF.mockImplementation(() => Promise.reject({ massage: 'Not Found'}))
             const req = mockRequest();
             const res = mockResponse();
-            await todoControllers.getTodos(req, res);
-            expect(res.status).toBe(400);
+            try {
+            await todoControllers.getTodos(req, res)
+            } catch(e){
+                expect(res.status).toHaveBeenCalledWith(400);
+                expect(res.send).toHaveBeenCalledWith({ message: 'Not Found' });
+            }
+
         })
     })
     describe('addTodos controller', () => {
         it('if status 200 return new Todo', async () => {
             mockF.mockImplementation(() => Promise.resolve(CREATE_SUCCESS_RESULT))
             let req = mockRequest();
-            req.params.name = 'Learn Spanish';
+            req.body.name = 'Learn Spanish';
             const res = mockResponse();
 
             await todoControllers.addTodo(req, res);
-            expect(res.send).toHaveBeenCalledTimes(1)
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(200);
 
         })
-        it('if status 400 return error', async () => {
-            mockF.mockImplementation(() => Promise.reject('Unable to add todo in list'))
+        it('if exist name and reject return status 400 and error', async () => {
+            mockF.mockImplementation(() => Promise.reject({message: 'Not found'}))
             let req = mockRequest();
-            req.params.name = 'Learn English';
+            req.body.name = 'Learn English';
             const res = mockResponse();
 
-            await todoControllers.addTodo(req, res);
-            console.log(res.status.mock)
-            expect(res.status).toHaveBeenCalledWith(400);
-            expect(res.send).toHaveBeenCalledWith(
-                'Unable to add todo in list');
+            try {
+                await todoControllers.addTodo(req, res);
+            } catch(e){
+                expect(e.status).toHaveBeenCalledWith(400);
+                expect(e.send).toHaveBeenCalledWith({message: 'Not found'});
+            }
+        })
+
+        it('if not exist name and reject return status 400 and error', async () => {
+            mockF.mockImplementation(() => Promise.resolve({message: 'Unable to create todo - empty name'}))
+            let req = mockRequest();
+            req.body.name = null;
+            const res = mockResponse();
+
+            try {
+                await todoControllers.addTodo(req, res);
+            }catch(e){
+                expect(e.status).toHaveBeenCalledWith(403);
+                expect(e.send).toHaveBeenCalledWith({ message: 'Not Found' });
+            }
+        })
+    })
+    describe('deleteTodo controller', () => {
+        it('if status 200 delete Todo by id', async () => {
+            mockF.mockImplementation(() => Promise.resolve(FIND_BY_ID_SUCCESS_RESULT))
+            let req = mockRequest();
+            req.params.id = 'ssdd-1212zxz-12lllfg';
+            const res = mockResponse();
+
+            await todoControllers.deleteTodo(req,res)
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(200);
+
+        })
+    })
+
+    describe('deleteCompleteTodo controller', () => {
+        it('if status 200 delete all todo with status true', async () => {
+            mockF.mockImplementation(() => Promise.resolve(DELETE_COMPLETE_TODO_SUCCESS_RESULT))
+            let req = mockRequest();
+            const res = mockResponse();
+
+            await todoControllers.deleteCompleteTodo(req,res)
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send.mock.calls[0][0].length).toBe(2);
+        })
+    })
+
+    describe('editTodo controller', () => {
+        it('if status 200 edit todo', async () => {
+            mockF.mockImplementation(() => Promise.resolve(EDIT_SUCCESS_RESULT))
+            let req = mockRequest();
+            req.params.id = '12dd-1212zxz-12lllfg';
+            req.body = {name: 'Study JS', status: true}
+            const res = mockResponse();
+
+            await todoControllers.editTodo(req,res)
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send.mock.calls[0].length).toBe(1);
+        })
+    })
+
+    describe('checkAllTodo controller', () => {
+        it('if status 200 change status all todo', async () => {
+            mockF.mockImplementation(() => Promise.resolve(CHECK_ALL_STATUS_COMPLETE_TODO_SUCCESS_RESULT))
+            let req = mockRequest();
+            req.body = {status: true}
+            const res = mockResponse();
+
+            await todoControllers.checkAllTodo(req,res)
+            expect(res.send).toHaveBeenCalledTimes(1);
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.send.mock.calls[0][0].length).toBe(2);
         })
     })
 })
